9e79569a3a6a30137f2b62d09eb7f59a20c04e75
iutkarsh2020 <utkarshsharma@Mac-1776.lan>
2025-05-20 21:22:41 -0400

    updares

--- loan_calculator/src/app/page.tsx
+++ loan_calculator/src/app/page.tsx
@@ -7,8 +7,13 @@ export enum states{
   LOAN_DENIED_STATE,
   PARSE_STATEMENT_STATE
 }
+interface FormHookState{
+  state: states,
+  message: string
+}
+
 export default function Home() {
-  const [currentState, changeCurrentState] = useState({state: states.LOAN_PROPOSAL_FORM, message: ""});
+  const [currentState, changeCurrentState] = useState({state: states.LOAN_PROPOSAL_FORM, message: ""} as FormHookState);
   return (
     <div className="h-[70%] w-screen flex justify-center items-center bg-slate-100">
       {currentState.state === states.LOAN_PROPOSAL_FORM && <LoanProposalCard changeCurrentState={changeCurrentState} contents={<LoanproposalForm changeCurrentState={changeCurrentState}/>}/> }
